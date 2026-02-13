b29c4c982e65d35fdf880bc7fedec7f81de3ab02
goofi0211 <34368047+goofi0211@users.noreply.github.com>
2026-02-14 03:17:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,8 @@ export default defineConfig(({ mode }) => {
         minify: 'terser',
       },
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY || env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_API_KEY || env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
